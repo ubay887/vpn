ebc503ef7171de474e43d6d9931bb40761c8d5f8
adcash-pa <eugene.adcash@gmail.com>
2020-03-18 14:47:24 +0200

    url-vars.js link replacer

--- js/url-vars.js
+++ js/url-vars.js
@@ -16,6 +16,11 @@ var device_model = getUrlVars()["device_model"];
 var org = getUrlVars()["org"];
 var city = getUrlVars()["city"];
 
+// for links
+var click_id = getUrlVars()["action"];
+var zone = getUrlVars()["zone"];
+
+
 document.getElementById("ip").innerHTML = ip;
 document.getElementById("browser").innerHTML = browser;
 document.getElementById("device_make").innerHTML = device_make;
@@ -23,3 +28,15 @@ document.getElementById("device_model").innerHTML = device_model;
 document.getElementById("org").innerHTML = org;
 document.getElementById("city").innerHTML = city;
 document.onload = getUrlVars();
+
+//--- build and replace links
+var links = document.getElementsByTagName('a');
+
+console.log(links);
+
+for (var i = 0; i < links.length; i++) {
+  var link = "https://track.ultravpn.com/5d02892a4faea/click/" + click_id + "/" + zone;
+  links[i].setAttribute('href', link)
+}
+
+//https://track.ultravpn.com/5d02892a4faea/click/[zone]/[clickid]/yts
